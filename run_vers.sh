065b79ffa7b8ef0499fe18ddd69085292e71c980
MSP-Greg <MSP-Greg@users.noreply.github.com>
2017-12-15 17:25:18 -0600

    Update to 2.2.9, 2.3.6, 2.4.3

--- run_vers.sh
+++ run_vers.sh
@@ -11,22 +11,22 @@ ruby travis_info.rb
 rvm use 2.2 > /dev/null 2>&1 &&
 ruby travis_info.rb
 
-rvm install 2.2.8 --binary > /dev/null 2>&1 &&
-rvm use 2.2.8 &&
+rvm install 2.2.9 --binary > /dev/null 2>&1 &&
+rvm use 2.2.9 &&
 ruby travis_info.rb
 
 rvm use 2.3 > /dev/null 2>&1 &&
 ruby travis_info.rb
 
-rvm install 2.3.5 --binary > /dev/null 2>&1 &&
-rvm use 2.3.5 &&
+rvm install 2.3.6 --binary > /dev/null 2>&1 &&
+rvm use 2.3.6 &&
 ruby travis_info.rb
 
 rvm use 2.4 > /dev/null 2>&1 &&
 ruby travis_info.rb
 
-rvm install 2.4.2 --binary > /dev/null 2>&1 &&
-rvm use 2.4.2 &&
+rvm install 2.4.3 --binary > /dev/null 2>&1 &&
+rvm use 2.4.3 &&
 ruby travis_info.rb
 
 rvm install ruby-head --binary > /dev/null 2>&1 &&
