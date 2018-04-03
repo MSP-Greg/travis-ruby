4302a23e62dad3d8871f62192bf1448880ce9c09
MSP-Greg <MSP-Greg@users.noreply.github.com>
2018-04-02 21:26:20 -0500

    2018-04-02 05:03 UTC

--- run_vers.sh
+++ run_vers.sh
@@ -12,28 +12,28 @@ echo rvm use 2.2
 rvm use 2.2 > /dev/null 2>&1 &&
 ruby travis_info.rb
 
-rvm install 2.2.9 --binary > /dev/null 2>&1 &&
-rvm use 2.2.9 &&
+rvm install 2.2.10 --binary > /dev/null 2>&1 &&
+rvm use 2.2.10 &&
 ruby travis_info.rb
 
 echo rvm use 2.3
 rvm use 2.3 > /dev/null 2>&1 &&
 ruby travis_info.rb
 
-rvm install 2.3.6 --binary > /dev/null 2>&1 &&
-rvm use 2.3.6 &&
+rvm install 2.3.7 --binary > /dev/null 2>&1 &&
+rvm use 2.3.7 &&
 ruby travis_info.rb
 
 echo rvm use 2.4
 rvm use 2.4 > /dev/null 2>&1 &&
 ruby travis_info.rb
 
-rvm install 2.4.3 --binary > /dev/null 2>&1 &&
-rvm use 2.4.3 &&
+rvm install 2.4.4 --binary > /dev/null 2>&1 &&
+rvm use 2.4.4 &&
 ruby travis_info.rb
 
-rvm install 2.5.0 --binary > /dev/null 2>&1 &&
-rvm use 2.5.0 &&
+rvm install 2.5.1 --binary > /dev/null 2>&1 &&
+rvm use 2.5.1 &&
 ruby travis_info.rb
 
 rvm install ruby-head --binary > /dev/null 2>&1 &&
