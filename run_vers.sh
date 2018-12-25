f9ddcca60ba625ee86f3b557a6f9e427c345dfc7
MSP-Greg <greg.mpls@gmail.com>
2018-12-25 11:08:12 -0600

    Addd 2.6.0

--- run_vers.sh
+++ run_vers.sh
@@ -19,39 +19,35 @@ rvm use ruby-2.0.0-p648 &&
 ruby travis_info.rb
 
 echo rvm use 2.1.10
-rvm install 2.1.10 --binary > /dev/null 2>&1 &&
-rvm use 2.1.10 &&
-ruby travis_info.rb
+rvm install  2.1.10 --binary > /dev/null 2>&1 &&
+rvm use      2.1.10 && ruby travis_info.rb
 
 echo rvm use 2.2
-rvm use 2.2 > /dev/null 2>&1 &&
-ruby travis_info.rb
+rvm use      2.2  > /dev/null 2>&1 && ruby travis_info.rb
 
-echo rvm use 2.2.10
-rvm install 2.2.10 --binary > /dev/null 2>&1 &&
-rvm use 2.2.10 &&
-ruby travis_info.rb
+rvm install  2.2.10 --binary > /dev/null 2>&1 &&
+rvm use      2.2.10 && ruby travis_info.rb
 
 echo rvm use 2.3
-rvm use 2.3 > /dev/null 2>&1 &&
-ruby travis_info.rb
+rvm use      2.3   > /dev/null 2>&1 && ruby travis_info.rb
 
-rvm install 2.3.7 --binary > /dev/null 2>&1 &&
-rvm use 2.3.7 &&
-ruby travis_info.rb
+rvm install  2.3.8 --binary > /dev/null 2>&1 &&
+rvm use      2.3.8 && ruby travis_info.rb
 
 echo rvm use 2.4
-rvm use 2.4 > /dev/null 2>&1 &&
-ruby travis_info.rb
+rvm use      2.4   > /dev/null 2>&1 && ruby travis_info.rb
 
-rvm install 2.4.5 --binary > /dev/null 2>&1 &&
-rvm use 2.4.5 &&
-ruby travis_info.rb
+rvm install  2.4.5 --binary > /dev/null 2>&1 &&
+rvm use      2.4.5 && ruby travis_info.rb
 
-rvm install 2.5.3 --binary > /dev/null 2>&1 &&
-rvm use 2.5.3 &&
-ruby travis_info.rb
+echo rvm use 2.5
+rvm use      2.5   > /dev/null 2>&1 && ruby travis_info.rb
+
+rvm install  2.5.3 --binary > /dev/null 2>&1 &&
+rvm use      2.5.3 && ruby travis_info.rb
+
+rvm install  2.6.0 --binary > /dev/null 2>&1 &&
+rvm use      2.6.0 && ruby travis_info.rb
 
 rvm install ruby-head --binary > /dev/null 2>&1 &&
-rvm use ruby-head &&
-ruby travis_info.rb
+rvm use ruby-head && ruby travis_info.rb
