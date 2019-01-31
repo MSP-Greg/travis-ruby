34eb3523da169e7eb7249db59d3cb89abc3fcdaf
MSP-Greg <greg.mpls@gmail.com>
2019-01-31 10:50:08 -0600

    Show both 2.6.0 & 2.6.1

--- run_vers.sh
+++ run_vers.sh
@@ -49,5 +49,8 @@ rvm use      2.5.3 && ruby travis_info.rb
 rvm install  2.6.0 --binary > /dev/null 2>&1 &&
 rvm use      2.6.0 && ruby travis_info.rb
 
+rvm install  2.6.1 --binary > /dev/null 2>&1 &&
+rvm use      2.6.1 && ruby travis_info.rb
+
 rvm install ruby-head --binary > /dev/null 2>&1 &&
 rvm use ruby-head && ruby travis_info.rb
